696d97b50a6570ea081650d2dd09e27ad49c73f2
mukilgs <61052517+mukilgs@users.noreply.github.com>
2022-03-22 11:55:39 +0530

    Update NewCreditCardApplication.java

--- NewCreditCard/src/main/java/com/example/project/NewCreditCardApplication.java
+++ NewCreditCard/src/main/java/com/example/project/NewCreditCardApplication.java
@@ -11,7 +11,7 @@ import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
-//import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 
 @EnableSwagger2
 @ComponentScan
